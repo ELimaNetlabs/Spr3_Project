ae75ca32198332be4c1820c4362014af50ea6442
Emiliano Lima <elima@netlabs.com.uy>
2025-01-14 12:15:20 -0300

    Se guarda esta version para rehacer todo el programa.

--- src/db/influxDB.py
+++ src/db/influxDB.py
@@ -1,4 +1,33 @@
-import influxdb_client
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+import time
 
-def run(bucketName):
-    return
+token = ""
+org = "Netlabs"
+url = "http://localhost:8086"
+client = InfluxDBClient(url=url, token=token, org=org)
+bucketName = ""
+
+def run(bName):
+    bucketName = bName
+    buckets_api = client.buckets_api()
+
+    bucket = buckets_api.create_bucket(
+        bucket_name=bucketName,
+        org=org,
+        retention_rules=[{
+            "type": "expire",
+            "every_seconds": 0
+        }]
+    )
+    print(f"Bucket '{bucket.name}' creado con éxito.")
+
+def cargarDatos(point):
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    #Esto llega por parametro
+    point = Point("temperatura") \
+    .tag("ubicacion", "cuarto") \
+    .field("valor", 27.3) 
+
+    write_api.write(bucket=bucketName, org="Netlabs",record=point)
--- src/main.py
+++ src/main.py
@@ -1,25 +1,43 @@
-from db import postgreSQL, influxDB
-from ui import grafana
+# from db import postgreSQL, influxDB
+# from ui import grafana
 
-#Levanto las bdd
-postgreSQL.run("psqlcivilizationsimulator")
-#influxDB.run("influxcivilizationsimulator")
+# #Levanto las bdd
+# postgreSQL.run("psqlcivilizationsimulator")
+# influxDB.run("influxcivilizationsimulator")
 
-#Agrego los Data Source
-postgrePayload = {
-    "name": "PostgreSQL-CivilizationSimulator",
-    "type": "postgres",
-    "url": "localhost:5432",
-    "access": "proxy",
-    "basicAuth": False,
-    "database": "psqlcivilizationsimulator",
-    "user": "emiliano",
-    "secureJsonData": {
-        "password": "Emiliano123"
-    },
-    "jsonData": {
-        "sslmode": "disable"  
-    }
-}
-grafana.addDataSource(postgrePayload)
-#grafana.editDashboard("fe9swpytiydq8c")
+# #Agrego los Data Source
+# postgrePayload = {
+#     "name": "PostgreSQL-CivilizationSimulator",
+#     "type": "postgres",
+#     "url": "localhost:5432",
+#     "access": "proxy",
+#     "basicAuth": False,
+#     "database": "psqlcivilizationsimulator",
+#     "user": "emiliano",
+#     "secureJsonData": {
+#         "password": "Emiliano123"
+#     },
+#     "jsonData": {
+#         "sslmode": "disable"  
+#     }
+# }
+# #grafana.addDataSource(postgrePayload)
+
+# influxPayload = {
+#     "name": "InfluxDB-CivilizationSimulator",  # Nombre del DataSource
+#     "type": "influxdb",  # Tipo de DataSource
+#     "url": "http://localhost:8086",  # URL de InfluxDB
+#     "access": "proxy",  # Método de acceso
+#     "isDefault": True,  # Hacerlo DataSource por defecto
+#     "jsonData": {
+#         "httpMode": "GET",  # Modo de petición (GET o POST)
+#         "timeInterval": "60s",  # Intervalo de tiempo por defecto
+#         "organization": "Netlabs",  # Organización de InfluxDB
+#         "bucket": "influxcivilizationsimulator",  # Bucket de InfluxDB
+#         "language" :"flux"
+#     },
+#     "secureJsonData": {
+#         "token": "QPHFtZ-X-ntdJNhqrf07T16cIOwtyIN0YRrb7diLW4VMK5uwScZd7offNrwpWeZznK-10WM1MXukFZmI6zZ4gw=="  # Token de autenticación de InfluxDB
+#     }
+# }
+# grafana.addDataSource(influxPayload)
